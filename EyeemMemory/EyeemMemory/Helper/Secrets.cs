ec695647f5df332fda005265a9001eaf781b0cab
Tobias Heine <spam@tobiasheine.eu>
2012-02-29 10:18:39 +0100

    Secret.cs

---
+++ EyeemMemory/EyeemMemory/Helper/Secrets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EyeemMemory.Helper
+{
+    public class Secrets
+    {
+        public static string Client_Url = "&client_id=YOUR_EYEEM_CLIENT_ID";
+
+    }
+}
